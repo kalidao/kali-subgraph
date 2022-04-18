c5da4914e83ff94406d19095e173b389a7f35474
Shiv <shivanshityagi@icloud.com>
2022-04-18 16:21:47 +0530

    add redeemables
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

---
+++ src/helpers/extension-helpers.ts
@@ -0,0 +1,11 @@
+import { Address, Bytes } from "@graphprotocol/graph-ts";
+import { KaliDAOredemption } from "../../generated/KaliDAOredemption/KaliDAOredemption";
+
+export function getRedeemables(dao: Address): Bytes[] {
+    const contract = KaliDAOredemption.bind(dao);
+
+    const redeemables = contract.try_getRedeemables(dao).value;
+    const redeemablesBytes = redeemables.map<Bytes>((redeemable) => redeemable as Bytes)
+    
+    return redeemablesBytes;
+}
--- src/mappings/redemption-mapping.ts
+++ src/mappings/redemption-mapping.ts
@@ -7,6 +7,7 @@ import {
   TokensRemoved as TokensRemovedEvent,
 } from '../../generated/KaliDAOredemption/KaliDAOredemption';
 import { Bytes } from '@graphprotocol/graph-ts';
+import { getRedeemables } from '../helpers/extension-helpers';
 
 // ExtensionSet
 export function handleExtensionSet(event: ExtensionSetEvent): void {
@@ -41,33 +42,30 @@ export function handleExtensionCalled(event: ExtensionCalledEvent): void {
 }
 // TokenAdded
 export function handleTokenAdded(event: TokensAddedEvent): void {
-  // const daoId = event.params.dao.toHexString();
-  // const redemptionId = daoId + '-redemption';
-  // let redemption = Redemption.load(redemptionId);
-  // if (redemption === null) {
-  //   redemption = new Redemption(redemptionId);
-  // }
-  // // const contract = KaliDAOredemption.bind(event.address);
-  // // const redeemables = contract.try_getRedeemables;
-  // redemption.save();
+  const daoId = event.params.dao.toHexString();
+  const redemptionId = daoId + '-redemption';
+  let redemption = Redemption.load(redemptionId);
+  
+  if (redemption === null) {
+    redemption = new Redemption(redemptionId);
+  }
+  
+  redemption.redeemables = getRedeemables(event.params.dao);
+  
+  redemption.save();
 }
 
 // Token Removed
 export function handleTokenRemoved(event: TokensRemovedEvent): void {
-  //   const daoId = event.params.dao.toHexString();
-  //   const redemptionId = daoId + '-redemption';
-  //   let redemption = Redemption.load(redemptionId);
-  //   if (redemption === null) {
-  //     redemption = new Redemption(redemptionId);
-  //   }
-  //   if (redemption.redeemables != null) {
-  //     const tokenIndex = event.params.tokenIndex;
-  //     const redeemables = redemption.redeemables;
-  //     const redeem = [];
-  //     for (let i = 0; i < tokenIndex.length; i++) {
-  //       redeem.push(redeemables[tokenIndex[i].toI32()]);
-  //     }
-  //     redemption.redeemed = redeem;
-  //   }
-  //   redemption.save();
+    const daoId = event.params.dao.toHexString();
+    const redemptionId = daoId + '-redemption';
+    let redemption = Redemption.load(redemptionId);
+    
+    if (redemption === null) {
+      redemption = new Redemption(redemptionId);
+    }
+
+    redemption.redeemables = getRedeemables(event.params.dao);
+
+    redemption.save();
 }
