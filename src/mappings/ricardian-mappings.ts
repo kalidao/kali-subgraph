e8af8856c28c887b0c6b213beca59f90de906fc4
Shiv <shivanshityagi@icloud.com>
2022-06-15 00:41:02 +0530

    setup
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

---
+++ src/mappings/ricardian-mappings.ts
@@ -0,0 +1,9 @@
+import {
+    Transfer as TransferEvent
+  } from '../../generated/KaliRicardian/Ricardian';
+
+export function handleTransferEvent(event: TransferEvent): void {
+  event.params.from 
+  
+  
+}  
