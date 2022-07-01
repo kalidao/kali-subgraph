c908a15595b8e9d35164c2fb946f7e55f94375a2
Shivanshi Tyagi <shivanshityagi@icloud.com>
2022-07-02 03:09:15 +0530

    ✨birthing project management
    Signed-off-by: Shivanshi Tyagi <shivanshityagi@icloud.com>

---
+++ src/mappings/project-management.ts
@@ -0,0 +1,25 @@
+import { Bytes } from '@graphprotocol/graph-ts'
+import { Project } from '../../generated/schema'
+import {
+  ExtensionSet as ExtensionSetEvent,
+  ExtensionCalled as ExtensionCalledEvent,
+} from '../../generated/ProjectManagement/ProjectManagement'
+
+// ExtensionSet
+export function handleExtensionSet(event: ExtensionSetEvent): void {
+  const daoId = event.params.dao.toHexString()
+  const projectId = daoId + '-project'
+  const project = new Project(projectId)
+
+  project.dao = daoId
+  project.serial = event.params.project[0].toBigInt()
+  project.manager = event.params.project[2].toBytes()
+  project.budget = event.params.project[3].toBigInt()
+  project.deadline = event.params.project[4].toBigInt()
+  project.goals = event.params.project[5].toString()
+
+  project.save()
+}
+
+// ExtensionCalled
+export function handleExtensionCalled(event: ExtensionCalledEvent): void {}
