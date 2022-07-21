4e4ce80b3cd73fa062c405321336340c7f01e580
Shivanshi Tyagi <shivanshityagi@icloud.com>
2022-07-21 12:46:26 +0530

    add callExtension
    Signed-off-by: Shivanshi Tyagi <shivanshityagi@icloud.com>

--- src/mappings/project-management.ts
+++ src/mappings/project-management.ts
@@ -1,5 +1,5 @@
-import { Bytes } from '@graphprotocol/graph-ts'
-import { Project } from '../../generated/schema'
+import { ethereum, log } from '@graphprotocol/graph-ts'
+import { Project, Contribution } from '../../generated/schema'
 import {
   ExtensionSet as ExtensionSetEvent,
   ExtensionCalled as ExtensionCalledEvent,
@@ -8,7 +8,7 @@ import {
 // ExtensionSet
 export function handleExtensionSet(event: ExtensionSetEvent): void {
   const daoId = event.params.dao.toHexString()
-  const projectId = daoId + '-project'
+  const projectId = daoId + '-project-' + event.params.project.id.toString()
   const project = new Project(projectId)
 
   project.active = true
@@ -24,4 +24,40 @@ export function handleExtensionSet(event: ExtensionSetEvent): void {
 }
 
 // ExtensionCalled
-export function handleExtensionCalled(event: ExtensionCalledEvent): void {}
+export function handleExtensionCalled(event: ExtensionCalledEvent): void {
+  const daoId = event.params.dao.toHexString()
+
+  for (let i = 0; i < event.params.updates.length; i++) {
+    const decoded = ethereum.decode('(uint256,address,uint256,string)', event.params.updates[i])
+    if (decoded !== null) {
+      const updates = decoded.toTuple()
+      const projectId = daoId + '-project-' + updates[0].toString()
+      const contributionId = projectId + '-contribution-' + updates[1].toString()
+      const contribution = new Contribution(contributionId)
+
+      contribution.project = updates[0].toString()
+      contribution.contributor = updates[1].toAddress()
+      contribution.remuneration = updates[2].toBigInt()
+      contribution.tribute = updates[4].toString()
+
+      log.info(
+        'Project decoded. DAO - {}, Tx Hash - {}, project - {}, contributor - {}, remuneration - {}, tribute - {}',
+        [
+          event.params.dao.toString(),
+          event.transaction.hash.toString(),
+          updates[0].toString(),
+          updates[1].toString(),
+          updates[2].toString(),
+          updates[4].toString(),
+        ],
+      )
+
+      contribution.save()
+    } else {
+      log.info('Project not decoded. DAO - {}, Tx Hash - {} ', [
+        event.params.dao.toString(),
+        event.transaction.hash.toString(),
+      ])
+    }
+  }
+}
